0c342617e3c8bf1dad386219aeb160c21ee4b750
JoanChege <jncheg@gmail.com>
2026-05-13 15:24:23 +0300

    Initial commit with day1

---
+++ day1/basic_username_generator.py
@@ -0,0 +1,5 @@
+# Learn how to print, input data, and use variables in Python.
+print("Hello, Welcome to the username generator!")
+pet = input("What is your pet's name? \n" )
+city = input("What is the city you grew up in? \n")
+print("Your username is:" +city + " " + pet )
